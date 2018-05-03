30f30125f817df5b478dec32d7b5dcb089551154
Chris Peragine <cperagine@gmail.com>
2018-05-03 09:42:34 -0400

    Adds Cond on symbol.txt

--- iex.py
+++ iex.py
@@ -3,6 +3,8 @@
 import sys
 import json
 import urllib.request
+import os
+
 
 # Global Static Variables for calls:
 LIST_BUILDER = "https://api.iextrading.com/1.0/ref-data/symbols"
@@ -11,11 +13,18 @@ NEWS_TAIL = "/news"
 
 
 def main():
+    # Create base news feed stocker list
     getStockList()
     return 0
 
+
 def getStockList():
-    
+    #check to see if file exists
+    if os.path.isfile('./symbols.txt'):
+        print("Exists!")
+    else:
+        print("Doesn't Exist")
+        
 
 if __name__ == "__main__":
     sys.exit(main())
