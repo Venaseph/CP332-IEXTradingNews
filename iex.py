a0c8255fffcc3618c23903b374cbb63d2962db43
Chris Peragine <cperagine@gmail.com>
2018-05-03 09:02:53 -0400

    First Commit

---
+++ iex.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# source ~/.bash_profile
+import sys
+
+
+def main():
+    getStockList()
+    return 0
+
+def getStockList():
+    print("")
+
+if __name__ == "__main__":
+    sys.exit(main())
