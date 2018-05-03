9cbc14d352b1494a01a7ff6d5f1f3dee4c8e0db6
Chris Peragine <cperagine@gmail.com>
2018-05-03 09:24:54 -0400

    Static Vars added for calls

--- iex.py
+++ iex.py
@@ -1,6 +1,13 @@
 # !/usr/bin/env python
 # source ~/.bash_profile
 import sys
+import json
+import urllib.request
+
+# Global Static Variables for calls:
+LIST_BUILDER = "https://api.iextrading.com/1.0/ref-data/symbols"
+NEWS_LEAD = "https://api.iextrading.com/1.0/stock/"
+NEWS_TAIL = "/news"
 
 
 def main():
@@ -8,7 +15,7 @@ def main():
     return 0
 
 def getStockList():
-    print("")
+    
 
 if __name__ == "__main__":
     sys.exit(main())
