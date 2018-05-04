4b1a60b786d5594898d5e0427212964871f556bf
Chris Peragine <cperagine@gmail.com>
2018-05-03 20:39:04 -0400

    Modification Check / List Creation

--- iex.py
+++ iex.py
@@ -4,12 +4,16 @@ import sys
 import json
 import urllib.request
 import os
+import datetime
+import time
 
 
 # Global Static Variables for calls:
-LIST_BUILDER = "https://api.iextrading.com/1.0/ref-data/symbols"
-NEWS_LEAD = "https://api.iextrading.com/1.0/stock/"
-NEWS_TAIL = "/news"
+LIST_BUILDER = 'https://api.iextrading.com/1.0/ref-data/symbols'
+NEWS_LEAD = 'https://api.iextrading.com/1.0/stock/'
+NEWS_TAIL = '/news'
+SYMBOLS_PATH = './symbols.txt'
+THIRTY_MINUTES = 10 * 60 * 1000
 
 
 def main():
@@ -19,12 +23,34 @@ def main():
 
 
 def getStockList():
-    #check to see if file exists
-    if os.path.isfile('./symbols.txt'):
-        print("Exists!")
+    # Check to see if file exists
+    if os.path.isfile(SYMBOLS_PATH):
+        # Check if time elapsed since symbols.py modification is +30 mins
+        if os.path.getmtime(SYMBOLS_PATH) < (THIRTY_MINUTES):
+            # Update List
+            createStockList()
+            print("made new stock list")
     else:
-        print("Doesn't Exist")
+        #Create List
+        createStockList()
+
         
+def createStockList():
+    # Create new symbol.txt, w allows overwrite
+    symFile = open("symbols.txt", "w")
+    # Open the URL url, which can be either a string or a Request object
+    fob = urllib.request.urlopen(LIST_BUILDER)
+    # Decode fob
+    data = fob.read().decode('utf-8')
+    # load json data from fob
+    symbols = json.loads(data)
+    # iterate through each dictonary to grab symbol and append to symbols.txt
+    for ticker in symbols:
+        # Kept two writes on seperate lines instead of (ticker['symbol'] + /n)
+        # which would create a new string in mem each time it runs.
+        symFile.write(ticker['symbol'])
+        symFile.write("\n")
+
 
 if __name__ == "__main__":
     sys.exit(main())
